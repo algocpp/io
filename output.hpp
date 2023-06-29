46204f6c73bfe77337066051a036526a628871bb
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2023-06-29 17:59:30 +0900

    boost

--- output.hpp
+++ output.hpp
@@ -5,6 +5,11 @@
 // If used, it must comply with the MIT License.
 
 #include <iostream>
+#include <map>
+#include <vector>
+#include <set>
+#include <locale>
+#include <codecvt>
 #include <algocpp/type/format.hpp>
 
 #ifndef ALGOCPP_IO_OUTPUT
@@ -28,6 +33,46 @@ std::ostream &operator<<(std::ostream &os, const std::pair<T1, T2> &p)
 
 template <typename T>
 std::ostream &operator<<(std::ostream &os, const std::set<T> &x)
+{
+	os << algocpp::type::format(x);
+
+	return os;
+}
+
+std::ostream &operator<<(std::ostream &os, const std::u32string &s)
+{
+	std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t> utf32conv;
+
+	os << utf32conv.to_bytes(s);
+
+	return os;
+}
+
+std::ostream &operator<<(std::ostream &os, const std::u16string &s)
+{
+	std::wstring_convert<std::codecvt_utf8<char16_t>, char16_t> utf16conv;
+
+	os << utf16conv.to_bytes(s);
+
+	return os;
+}
+
+std::ostream &operator<<(std::ostream &os, const char32_t &c)
+{
+	os << std::u32string{c};
+
+	return os;
+}
+
+std::ostream &operator<<(std::ostream &os, const char16_t &c)
+{
+	os << std::u16string{c};
+
+	return os;
+}
+
+template <typename T1, typename T2>
+std::ostream &operator<<(std::ostream &os, const std::map<T1, T2> &x)
 {
 	os << algocpp::type::format(x);
 
