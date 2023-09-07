4849287ace88e1f753e17bd7cebab3581e59f190
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2023-09-07 21:09:59 +0900

    要素が空でも`std::vector`を入力できるように

--- input.hpp
+++ input.hpp
@@ -8,14 +8,35 @@
 #define ALGOCPP_IO_INPUT
 
 #include <iostream>
+#include <sstream>
 #include <vector>
+#include <string>
 
 template <typename T>
 std::istream &operator>>(std::istream &os, std::vector<T> &v)
 {
-	for (unsigned long long i = 0; i < v.size(); i++)
+	if (v.size() != 0)
 	{
-		os >> v[i];
+		for (unsigned long long i = 0; i < v.size(); i++)
+		{
+			os >> v[i];
+		}
+	}
+	else
+	{
+		std::string inp;
+		std::stringstream ss;
+
+		std::getline(os, inp);
+		ss << inp;
+
+		T tmp;
+		while (!ss.eof())
+		{
+			ss >> tmp;
+
+			v.emplace_back(tmp);
+		}
 	}
 
 	return os;
