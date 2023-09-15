ce357d8370e1f518489b5ccf486555cf5e742b2e
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2023-09-15 12:38:49 +0900

    その他もろもろ

--- input.hpp
+++ input.hpp
@@ -10,16 +10,18 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
+#include <array>
+#include <list>
 #include <string>
 
 template <typename T>
-std::istream &operator>>(std::istream &os, std::vector<T> &v)
+std::istream &operator>>(std::istream &is, std::vector<T> &v)
 {
 	if (v.size() != 0)
 	{
 		for (unsigned long long i = 0; i < v.size(); i++)
 		{
-			os >> v[i];
+			is >> v[i];
 		}
 	}
 	else
@@ -27,10 +29,12 @@ std::istream &operator>>(std::istream &os, std::vector<T> &v)
 		std::string inp;
 		std::stringstream ss;
 
-		std::getline(os, inp);
+		std::getline(is, inp);
 		ss << inp;
 
 		T tmp;
+		v.clear();
+
 		while (!ss.eof())
 		{
 			ss >> tmp;
@@ -39,7 +43,37 @@ std::istream &operator>>(std::istream &os, std::vector<T> &v)
 		}
 	}
 
-	return os;
+	return is;
+}
+
+template <typename T, std::size_t N>
+std::istream &operator>>(std::istream &is, std::array<T, N> &v)
+{
+	for (unsigned long long i = 0; i < v.size(); i++)
+	{
+		is >> v[i];
+	}
+
+	return is;
+}
+
+template <typename T>
+std::istream &operator>>(std::istream &is, std::list<T> &v)
+{
+	std::string inp;
+	std::stringstream ss;
+
+	getline(is, inp);
+	ss << inp;
+
+	T tmp;
+	v.clear();
+
+	while (!ss.eof())
+	{
+		is >> tmp;
+		v.emplace_back(tmp);
+	}
 }
 
 #endif // ALGOCPP_IO_INPUT
